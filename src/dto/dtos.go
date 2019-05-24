db1d11a7335d71687c9d4618d49b79d1e88c04b4
hyuugatt <h121arx8@gmail.com>
2019-05-24 17:33:08 +0900

    dto修正

--- src/dto/dtos.go
+++ src/dto/dtos.go
@@ -2,22 +2,22 @@ package dto
 
 import (
 	"time"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 type TUserProfile struct {
-	gorm.Model
-	UserId      string    `json:user_id`
-	UserName    string    `json:user_name`
-	Birthday    time.Time `json:birthday`
-	MailAddress string    `json:mail_address`
-	Password    string    `json:password`
-	Profile     string    `json:profile`
-	RegistData  time.Time `json:regist_data`
-	CreateUser  string    `json:create_user`
-	CreateDate  time.Time `json:create_date`
-	UpdateUser  string    `json:update_user`
-	UpdateDate  time.Time `json:update_date`
+	UserId      string    `json:"user_id"`
+	UserName    string    `json:"user_name"`
+	Birthday    time.Time `json:"birthday"`
+	MailAddress string    `json:"mail_address"`
+	Password    string    `json:"password"`
+	Profile     string    `json:"profile"`
+	RegistData  time.Time `json:"regist_data"`
+	CreateUser  string    `json:"create_user"`
+	CreateDate  time.Time `json:"create_date"`
+	UpdateUser  string    `json:"update_user"`
+	UpdateDate  time.Time `json:"update_date"`
+}
+
+func (m *TUserProfile) TableName() string {
+	return "t_user_profile"
 }
